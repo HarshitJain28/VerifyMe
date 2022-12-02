9b2fd99ee13e00f98d5864c46ef4d52012aa723c
HarshitJain28 <harshit.nj@somaiya.edu>
2022-12-03 02:21:28 +0530

    Final Commit

--- contracts/VerifyMe.sol
+++ contracts/VerifyMe.sol
@@ -1,14 +1,18 @@
 pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
+
 
 contract VerifyMe {
-    uint256 userCount = 0;
-    uint256 skillCount = 0;
+    uint256 public userCount = 0;
+    uint256 public skillCount = 0;
 
     struct Skill {
         uint256 skill_id;
         string name;
         uint256 level;
         uint256 endorsements;
+        address user;
+        address[] endorsed_by;
     }
 
     struct User {
@@ -16,7 +20,6 @@ contract VerifyMe {
         string name;
         address waddress;
         string email;
-        uint256[] user_skills;
     }
 
     mapping(address => User) public users;
@@ -24,24 +27,33 @@ contract VerifyMe {
 
     function setEmail(string memory _email, string memory _name) public {
         require(msg.sender != users[msg.sender].waddress);
-        User memory user = User({user_id: userCount, name: _name, waddress: msg.sender, email: _email, user_skills: new uint256[](0)});
+        User memory user = User({user_id: userCount, name: _name, waddress: msg.sender, email: _email});
         users[msg.sender] = user;
         userCount++;
     }
 
     function addSkill(string memory _name, uint256 _level) public {
         User storage user = users[msg.sender];
-        Skill memory skill = Skill({skill_id: skillCount, name: _name, level: _level, endorsements: 0});
-        user.user_skills.push(skillCount);
+        Skill memory skill = Skill({skill_id: skillCount, name: _name, level: _level, endorsements: 0, user: msg.sender, endorsed_by: new address[](0)});
         skills[skillCount] = skill;
         skillCount++;
     }
 
+    function endorsement_exists(address[] memory endorsed_by) private returns (bool) {
+    for (uint i = 0; i < endorsed_by.length; i++) {
+        if (endorsed_by[i] == msg.sender) {
+            return true;
+        }
+    }
+        return false;
+    }
+
     function endorseSkill(uint256 _id) public {
         Skill storage skill = skills[_id];
+        require(!endorsement_exists(skill.endorsed_by),"Endorsement already exists");
         skill.endorsements++;
+        skill.endorsed_by.push(msg.sender);
         skills[_id] = skill;
 
     }
-
 }
--- src/App.js
+++ src/App.js
@@ -59,7 +59,102 @@ App = {
     render: async () => {    
       // Render Account
       console.log(App.account);
+      await App.checkUser();
     },
+
+    checkUser: async () => {
+      const user = await App.verifyMe.users(App.account);
+      console.log(user);
+      if(user[1] != ''){
+        console.log('Exists');
+        App.renderMySkills();
+        App.renderAllSkills();
+      } else {
+        console.log('New');
+        $('#newUserModal').modal('show');
+      }
+    },
+    
+    createUser: async () => {
+      uname = $('#userName').val();
+      uemail = $('#userEmail').val();
+      await App.verifyMe.setEmail(uemail, uname,{from:App.account,gas:3000000})
+      window.location.reload()
+    },
+
+    addSkill: async () => {
+      sname = $('#skillName').val();
+      slevel = $('input[name="skillLevel"]:checked').val();
+      await App.verifyMe.addSkill(sname, slevel,{from:App.account,gas:3000000})
+      $('#skillName').val('');
+      Swal.fire({
+        icon: 'success',
+        title: 'Skill Added successfully',
+        confirmButtonText: 'Cool',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.reload()
+        } 
+      })
+    },
+
+    renderMySkills: async () => {
+      const skillCount = await App.verifyMe.skillCount();
+      const $mmyskillTemplate = $('.myskillTemplate')
+      for (let index = 0; index < skillCount['c'][0]; index++) {
+        const skill = await App.verifyMe.skills(index);
+        console.log(skill)
+        if(skill[4] == App.account){
+          const $myskilltemp = $mmyskillTemplate.clone()
+          $myskilltemp.find('.myskill_name').html(skill[1])
+          $myskilltemp.find('.myskill_level').html(skill[2]['c'][0])
+          $myskilltemp.find('.myskill_end').html(skill[3]['c'][0])
+          $('#mySkills').append($myskilltemp);
+          $myskilltemp.show()
+        }
+        
+      }
+    },
+    renderAllSkills: async () => {
+      const skillCount = await App.verifyMe.skillCount();
+      const $skillTemplate = $('.skillTemplate')
+      for (let index = 0; index < skillCount['c'][0]; index++) {
+        const skill = await App.verifyMe.skills(index);
+        if(skill[4] != App.account){
+          const $skilltemp = $skillTemplate.clone()
+          const user = await App.verifyMe.users(skill[4])
+          $skilltemp.find('.skill_user_name').html(user[1])
+          $skilltemp.find('.skill_user_email').html(user[3])
+          $skilltemp.find('.skill_name').html(skill[1])
+          $skilltemp.find('.skill_level').html(skill[2]['c'][0])
+          $skilltemp.find('.skill_end').html(skill[3]['c'][0])
+          $skilltemp.find('.endor').html(`<button class="btn btn-primary" onClick="App.endorseSkill(`+index+`)">Endorse</button>`)
+          $('#Skills').append($skilltemp);
+          $skilltemp.show()
+        }
+        
+      }
+    },
+    endorseSkill: async (index) => {
+      try {
+        await App.verifyMe.endorseSkill(index,{from:App.account,gas:3000000})
+        Swal.fire({
+          icon: 'success',
+          title: 'Skill Endorsed successfully',
+          confirmButtonText: 'Cool',
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.reload()
+          } 
+        })
+      } catch (error) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Skill is already endorsed by you',
+        })
+      }
+      }
 
     
 }
